a3712b4548bb7ff1369b0a318dc5eea6476a3118
Harsh Bothra <harsh.love.619@gmail.com>
2023-01-05 14:22:08 +0530

    Added Prod Config

--- HudsonLearning/Program.cs
+++ HudsonLearning/Program.cs
@@ -1,5 +1,7 @@
+using HudsonLearning.Data;
 using HudsonLearning.Extensions;
 using HudsonLearning.Middlewares;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,5 +34,17 @@ app.MapControllerRoute(
     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html");
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    app.Logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
